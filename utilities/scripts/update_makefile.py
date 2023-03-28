2424210b1f14e676292f771abf75e73262d24932
budgettsfrog <budgettsfrog@protonmail.com>
2023-03-28 06:59:00 -0700

    sting paths will be the death of me

--- utilities/scripts/update_makefile.py
+++ utilities/scripts/update_makefile.py
@@ -5,7 +5,7 @@ def update_makefile(makefile_path, original_path, new_path):
     with open(makefile_path, 'r') as file:
         filedata = file.read()
 
-    filedata = re.sub(re.escape(original_path), new_path, filedata)
+    filedata = re.sub(re.escape(original_path), re.escape(new_path), filedata)
 
     with open(makefile_path, 'w') as file:
         file.write(filedata)
@@ -14,8 +14,8 @@ makefile_path = 'implementations/samd20-optiboot-custom/samd20-optiboot-custom/D
 original_path = r'C:\Program Files (x86)\Atmel\Studio\7.0\toolchain\arm\arm-gnu-toolchain\bin'
 new_path = r'C:\ProgramData\chocolatey\lib\gcc-arm-embedded\tools\bin'
 
-cmsis_path = os.path.join(os.getcwd(), "CMSIS_5.4.0", "CMSIS", "Core", "Include")
-samd20_dfp_path = os.path.join(os.getcwd(), "SAMD20_DFP", "1.3.165", "samd20", "include")
+cmsis_path = os.path.join(os.getcwd(), r"CMSIS_5.4.0\CMSIS\Core\Include")
+samd20_dfp_path = os.path.join(os.getcwd(), r"SAMD20_DFP\1.3.165\samd20\include")
 original_cmsis_path = r'C:\Program Files (x86)\Atmel\Studio\7.0\Packs\arm\CMSIS\5.4.0\CMSIS\Core\Include'
 original_samd20_dfp_path = r'C:\Program Files (x86)\Atmel\Studio\7.0\Packs\atmel\SAMD20_DFP\1.3.165\samd20\include'
 
