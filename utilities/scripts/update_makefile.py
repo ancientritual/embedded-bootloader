08233995a159bcf10d91104a6689f95e7440c045
budgettsfrog <budgettsfrog@protonmail.com>
2023-03-28 06:43:22 -0700

    right DFP version

--- utilities/scripts/update_makefile.py
+++ utilities/scripts/update_makefile.py
@@ -15,7 +15,7 @@ original_path = r'C:\Program Files (x86)\Atmel\Studio\7.0\toolchain\arm\arm-gnu-
 new_path = r'C:\ProgramData\chocolatey\lib\gcc-arm-embedded\tools\bin'
 
 cmsis_path = os.path.join(os.getcwd(), "CMSIS_5.4.0", "CMSIS", "Core", "Include")
-samd20_dfp_path = os.path.join(os.getcwd(), "SAMD20_DFP", "1.3.415", "samd20", "include")
+samd20_dfp_path = os.path.join(os.getcwd(), "SAMD20_DFP", "1.3.165", "samd20", "include")
 original_cmsis_path = r'C:\Program Files (x86)\Atmel\Studio\7.0\Packs\arm\CMSIS\5.4.0\CMSIS\Core\Include'
 original_samd20_dfp_path = r'C:\Program Files (x86)\Atmel\Studio\7.0\Packs\atmel\SAMD20_DFP\1.3.165\samd20\include'
 
