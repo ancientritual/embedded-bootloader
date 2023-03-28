1ef028be09bd1cd6c5e467f2a5d170bc7574c2dd
budgettsfrog <budgettsfrog@protonmail.com>
2023-03-28 05:36:39 -0700

    Try using the makefile and blasting the paths

---
+++ utilities/scripts/update_makefile.py
@@ -0,0 +1,18 @@
+import os
+import re
+
+def update_makefile(makefile_path, original_path, new_path):
+    with open(makefile_path, 'r') as file:
+        filedata = file.read()
+
+    filedata = re.sub(re.escape(original_path), new_path, filedata)
+
+    with open(makefile_path, 'w') as file:
+        file.write(filedata)
+
+if __name__ == "__main__":
+    makefile_path = 'implementations/samd20-optiboot-custom/samd20-optiboot-custom/Debug/Makefile'
+    original_path = r'C:\Program Files (x86)\Atmel\Studio\7.0\toolchain\arm\arm-gnu-toolchain\bin'
+    new_path = r'C:\ProgramData\chocolatey\lib\gcc-arm-embedded\tools\bin'
+
+    update_makefile(makefile_path, original_path, new_path)
