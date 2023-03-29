69139b5f772bfecf44e7b3c81175a4f7eb329102
budgettsfrog <budgettsfrog@protonmail.com>
2023-03-29 09:04:37 -0700

    Major version release. Added a powershell get_version script so that we can version the release asset in the releases. Produce a true tarball.

--- src/bl_version.h
+++ src/bl_version.h
@@ -1,8 +1,8 @@
 #ifndef BL_VERSION_H
 #define BL_VERSION_H
 
-#define BL_VERSION_MAJOR    0
+#define BL_VERSION_MAJOR    1
 #define BL_VERSION_MINOR    0
-#define BL_VERSION_REV      74
+#define BL_VERSION_REV      2
 
 #endif /* BL_VERSION_H */
